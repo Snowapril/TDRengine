eebf9a41794b66c94baafd4c69fc9586305bff24
snowapril <sinjihng@gmail.com>
2022-11-13 19:36:44 +0900

    setup(project): discoard original ones and move to rust project

--- Sources/RHI/RHIRenderDevice.cpp
+++
@@ -1,8 +0,0 @@
-// (c) 2022 Snowapril
-// This code is licensed under MIT license (see LICENSE.txt for details)
-
-#include <RHI/RHIRenderDevice.h>
-
-namespace TDR
-{
-}  // namespace TDR
--- Sources/RHI/TDRengine.cpp
+++
@@ -1,8 +0,0 @@
-// (c) 2022 Snowapril
-// This code is licensed under MIT license (see LICENSE.txt for details)
-
-#include <RHI/TDRengine.h>
-
-namespace TDR
-{
-}  // namespace TDR
